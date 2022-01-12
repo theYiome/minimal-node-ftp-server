a70a8674a7b934fd87055757a94df08ab66c1e69
Kamil Pasterczyk <yiome@outlook.com>
2022-01-12 11:21:42 +0100

    login implemented

---
+++ src/files.ts
@@ -0,0 +1,38 @@
+import * as fsPromises from 'node:fs/promises';
+
+export async function loadString(path: string): Promise<string> {
+    try {
+        // https://nodejs.org/api/fs.html
+        const file = await fsPromises.open(path, "r");
+        const buffer = await file.readFile();
+        // console.log(fileData, fileData.toJSON(), fileData.toString());
+        // fileData.
+        file.close();
+        // const buffer = fileData.buffer.slice(0, fileData.bytesRead);
+        return buffer.toString();
+    }
+    catch (err) {
+        console.warn(err);
+        throw `Failed to load: ${path}`;
+    }
+}
+
+
+export async function loadJSON(path: string) {
+    return JSON.parse(await loadString(path));
+}
+
+
+export async function saveJSON(object: any, path: string, indent = 0) {
+    try {
+        // https://nodejs.org/api/fs.html
+        const file = await fsPromises.open(path, "w");
+        file.write(JSON.stringify(object, null, indent));
+        file.close();
+    }
+    catch (err) {
+        const errorStr = `Failed to write to: ${path}`;
+        console.warn(errorStr, err);
+        throw errorStr;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
-const telnet = require('telnet');
+import { loadJSON } from './files';
+import * as fsPromises from 'node:fs/promises';
 
+type User = {
+    username: string;
+    password: string;
+};
+
+let users: User[] = null;
+
+loadJSON("users.json").then(file => {
+    users = file.users;
+
+    const storageDir = "storage";
+    fsPromises.mkdir(storageDir, { recursive: true });
+    users.forEach(user => {
+        const mainUserDir = "storage/" + user.username;
+        fsPromises.mkdir(mainUserDir, { recursive: true })
+
+        const publicUserDir = mainUserDir + "/public";
+        fsPromises.mkdir(publicUserDir, { recursive: true })
+    });
+});
+
+
+const telnet = require('telnet');
 telnet.createServer(function (client: any) {
 
     // make unicode characters work properly
@@ -7,14 +31,16 @@ telnet.createServer(function (client: any) {
 
     // listen for the actual data from the client
     client.on('data', function (b: Buffer) {
-        const data: string = b.toString();
-        const command = data.split(" ")[0];
-        const value = data.split(" ")[1];
-        console.log({ data, command });
+        const data: string = b.toString().trim();
+        const dataSplitted = data.split(" ");
+        const command = dataSplitted[0];
+        const value = dataSplitted[1];
+
+        console.log({ data, value, command });
 
         switch (command) {
             case "OPTS": {
-                client.write(responses.ready);
+                client.write(responses.ok);
                 break;
             }
             case "USER": {
@@ -24,12 +50,45 @@ telnet.createServer(function (client: any) {
                 break;
             }
             case "PASS": {
-                client.write(responses.loggedIn);
-                client.loginSuccessful = true;
+                const currentUser = users.find(user => user.username === client.username);
+                const passwordCorrect = currentUser ? currentUser.password === value : false;
+                if(currentUser && passwordCorrect) {
+                    client.write(responses.loggedIn);
+                    client.loginSuccessful = true;
+                }
+                else {
+                    client.write(responses.loginFailed);
+                    client.loginSuccessful = false; 
+                }
+                break;
+            }
+            case "PORT": {
+                client.write(responses.ok);
+                client.port = value;
+                break;
+            }
+            case "XPWD": {
+                client.write(responses.ok);
+                break;
+            }
+            case "NLST": {
+                client.write(responses.ok);
+                break;
+            }
+            case "CWD": {
+                client.write(responses.ok);
+                break;
+            }
+            case "STOR": {
+                client.write(responses.ok);
+                break;
+            }
+            case "RETR": {
+                client.write(responses.ok);
                 break;
             }
             default: {
-                console.log(client.username);
+                console.warn(`${command} not implemented!`);
                 client.write(responses.notImplemented); 
                 break;
             }
@@ -37,20 +96,27 @@ telnet.createServer(function (client: any) {
 
     });
 
+    client.write(responses.ready);
+
 }).listen(21);
 
 
 const responses = {
     fileOk: "150 File ok, about to open data connection.\n",
+
     ok: "200 OK.\n",
     ready: "220 Service ready for new user.\n",
+    disconnected: "221 Disconnected.\n",
     closingControlConnection: "221 Service closing control connection.\n",
     actionSuccessful: "226 Closing data connection. Requested file action successful.\n",
     loggedIn: "230 Logged in.\n",
+
     passwordRequired: "331 Password required for username.\n",
     cantOpen: "425 Can't open data connection.\n",
     connectionClosed: "426 Connection closed; transfer aborted.\n",
+
     syntaxError: "501 Syntax error in parameters or arguments.\n",
     notImplemented: "502 Not implemented.\n",
+    loginFailed: "530 User cannot log in.\n",
     fileUnavailable: "550 Requested action not taken. File unavailable.\n"
 }
